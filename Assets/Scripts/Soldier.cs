26ffd52d18927fa2d96f4b4fd5c38ce3ea8476e4
Allan Thang <allan.thang1920@gmail.com>
2023-03-01 23:08:43 +1100

    Logic for collision with Tree
    Moved soldiers and hospital logic to HelicopterController

--- Assets/Scripts/Soldier.cs
+++ Assets/Scripts/Soldier.cs
@@ -5,21 +5,5 @@ using UnityEngine;
 
 public class Soldier : MonoBehaviour
 {
-    [SerializeField] private SpriteRenderer spriteRenderer;
-    private bool _pickedUp;
-    private HelicopterController _helicopter;
-
-    private void OnTriggerEnter2D(Collider2D trigCollider2D)
-    {
-        // Check if collided obj is the player
-        if (!trigCollider2D.gameObject.CompareTag("Player")) return;
-        _helicopter = trigCollider2D.GetComponent<HelicopterController>();
-
-        // Check if Helicopter is full and if the soldier was already picked up
-        if (_helicopter.soldiers.Count >= _helicopter.maxSoldiers || _pickedUp) return;
-        _pickedUp = true; // Make sure it can't be counted twice
-        spriteRenderer.enabled = false; // Make it disappear
-        trigCollider2D.GetComponent<HelicopterController>().soldiers.Add(this); // Add the Soldier to the Helicopter
-        // Update the tracker
-    }
+    public bool pickedUp;
 }
